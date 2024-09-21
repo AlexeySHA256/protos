74d5bab372d567540078f1216b7e828a6534112d
AlexeyTarasov77 <alexxey2707@gmail.com>
2024-09-21 08:50:51 +0200

    New rpc go get new activation token

--- gen/go/sso/sso_grpc.pb.go
+++ gen/go/sso/sso_grpc.pb.go
@@ -19,13 +19,14 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Auth_Register_FullMethodName         = "/auth.Auth/Register"
-	Auth_Login_FullMethodName            = "/auth.Auth/Login"
-	Auth_IsAdmin_FullMethodName          = "/auth.Auth/IsAdmin"
-	Auth_GetOrCreateApp_FullMethodName   = "/auth.Auth/GetOrCreateApp"
-	Auth_ActivateUser_FullMethodName     = "/auth.Auth/ActivateUser"
-	Auth_RenewAccessToken_FullMethodName = "/auth.Auth/RenewAccessToken"
-	Auth_GetUser_FullMethodName          = "/auth.Auth/GetUser"
+	Auth_Register_FullMethodName           = "/auth.Auth/Register"
+	Auth_Login_FullMethodName              = "/auth.Auth/Login"
+	Auth_IsAdmin_FullMethodName            = "/auth.Auth/IsAdmin"
+	Auth_GetOrCreateApp_FullMethodName     = "/auth.Auth/GetOrCreateApp"
+	Auth_ActivateUser_FullMethodName       = "/auth.Auth/ActivateUser"
+	Auth_RenewAccessToken_FullMethodName   = "/auth.Auth/RenewAccessToken"
+	Auth_GetUser_FullMethodName            = "/auth.Auth/GetUser"
+	Auth_NewActivationToken_FullMethodName = "/auth.Auth/NewActivationToken"
 )
 
 // AuthClient is the client API for Auth service.
@@ -39,6 +40,7 @@ type AuthClient interface {
 	ActivateUser(ctx context.Context, in *ActivateUserRequest, opts ...grpc.CallOption) (*ActivateUserResponse, error)
 	RenewAccessToken(ctx context.Context, in *RenewAccessTokenRequest, opts ...grpc.CallOption) (*RenewAccessTokenResponse, error)
 	GetUser(ctx context.Context, in *GetUserRequest, opts ...grpc.CallOption) (*GetUserResponse, error)
+	NewActivationToken(ctx context.Context, in *NewActivationTokenRequest, opts ...grpc.CallOption) (*NewActivationTokenResponse, error)
 }
 
 type authClient struct {
@@ -119,6 +121,16 @@ func (c *authClient) GetUser(ctx context.Context, in *GetUserRequest, opts ...gr
 	return out, nil
 }
 
+func (c *authClient) NewActivationToken(ctx context.Context, in *NewActivationTokenRequest, opts ...grpc.CallOption) (*NewActivationTokenResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(NewActivationTokenResponse)
+	err := c.cc.Invoke(ctx, Auth_NewActivationToken_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AuthServer is the server API for Auth service.
 // All implementations must embed UnimplementedAuthServer
 // for forward compatibility.
@@ -130,6 +142,7 @@ type AuthServer interface {
 	ActivateUser(context.Context, *ActivateUserRequest) (*ActivateUserResponse, error)
 	RenewAccessToken(context.Context, *RenewAccessTokenRequest) (*RenewAccessTokenResponse, error)
 	GetUser(context.Context, *GetUserRequest) (*GetUserResponse, error)
+	NewActivationToken(context.Context, *NewActivationTokenRequest) (*NewActivationTokenResponse, error)
 	mustEmbedUnimplementedAuthServer()
 }
 
@@ -160,6 +173,9 @@ func (UnimplementedAuthServer) RenewAccessToken(context.Context, *RenewAccessTok
 }
 func (UnimplementedAuthServer) GetUser(context.Context, *GetUserRequest) (*GetUserResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetUser not implemented")
+}
+func (UnimplementedAuthServer) NewActivationToken(context.Context, *NewActivationTokenRequest) (*NewActivationTokenResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method NewActivationToken not implemented")
 }
 func (UnimplementedAuthServer) mustEmbedUnimplementedAuthServer() {}
 func (UnimplementedAuthServer) testEmbeddedByValue()              {}
@@ -308,6 +324,24 @@ func _Auth_GetUser_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Auth_NewActivationToken_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(NewActivationTokenRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).NewActivationToken(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Auth_NewActivationToken_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).NewActivationToken(ctx, req.(*NewActivationTokenRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Auth_ServiceDesc is the grpc.ServiceDesc for Auth service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -342,6 +376,10 @@ var Auth_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetUser",
 			Handler:    _Auth_GetUser_Handler,
+		},
+		{
+			MethodName: "NewActivationToken",
+			Handler:    _Auth_NewActivationToken_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
